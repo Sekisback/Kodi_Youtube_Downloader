fb030809d5bb04b1e2f77f72f1b73140dafad982
SekIsBack <sek_is_back@gmx.de>
2021-01-01 12:06:35 +0100

    initial

---
+++ README.md
@@ -0,0 +1,23 @@
+# Youtube Video Downloader for Kodi
+============
+Downloads a Youtube Video in highest resolution and convert the Video to mkv.
+Downloads the corresponding Image and create a episode nfo File for Kodi.
+
+# Kodi NFO File
+============
+title
+showtitle
+aired
+runtime
+thumb
+
+Requirements
+============
+
+ * A Python interpreter. Versions 3 and newer are known to work.
+ * pytube
+ * wget
+ * xml
+ * os
+ 
+ * ffmpeg
---
+++ kodi-youtube-downloader.py
@@ -0,0 +1,99 @@
+# YoutubeVideo Downloader fro Kodi V1.0
+# Downloads a YoutubeVideo andthe corresponding Image
+# Convert the Video from Mp4 to MKV and create a Kodi episode nfo file
+
+
+import os
+import xml.etree.cElementTree as ET
+import wget
+from pytube import YouTube
+
+x_name = "WORKOUT"
+x_session = "03"
+x_episode = "07"
+path = "Downloads/"
+
+# Step 1 Get Input
+def get_input(x_name, x_session, x_episode):
+    vurl = input('YouTube-URL: ')
+    name = input(f'Serienname (default = {x_name}): ')
+    if name == '': name = (x_name)
+    session = input(f'Staffel (default = {x_session}): ')
+    if session == '': session = (x_session)
+    episode = input(f'Episode (default = {x_episode}): ')
+    if episode == '': episode = (x_episode)
+    prefix = (f"{name} - S{session}E{episode} - ")
+    return (prefix, vurl)
+
+# Step 2 Get Video, Image and extract duration and publish date
+def get_video(vurl, path, prefix):
+    yt = YouTube(vurl)
+    title = (prefix + (yt.title.split('|')[0]).strip().replace('.', '').replace('&', 'and').strip())
+    lenght = str(yt.length//60)
+    publish = yt.publish_date
+    air = publish.strftime('%Y-%m-%d')
+    stream = yt.streams.get_highest_resolution()
+    print('\n[+] Downloading ' + yt.title)
+    stream.download(output_path=(path), filename=(title))
+
+    print('[+] Downloading Image')
+    image_url = yt.thumbnail_url
+    image_name = (title + '.jpg')
+    wget.download(image_url, bar=None, out=(path + image_name))
+    return(lenght, air)
+
+# Step 3 Convert Mp4 to MKV
+def convert_video(path, prefix):
+    print('[+] Converting MP4 to MKV')
+    video = [video for video in os.listdir(path=path) if video.endswith('.mp4')]                
+    video_in = repr(video[0])
+    video_out = str(video_in.replace('.mp4', '.mkv'))
+    prefix = repr(prefix)
+    os.system('ffmpeg -i {0} -c:v copy -c:a copy {1} > /dev/null 2>&1'.format(path+video_in, path+video_out))                                            # convert the Video without encoding
+    return(video_out[1:-5])
+
+# Step 4 Create xml.nfo File
+def xml_output(path, video, lenght, air):
+    print('[+] Creating NFO-File')
+    episodedetails = ET.Element('episodedetails')
+    title = ET.SubElement(episodedetails, "title")
+    title.text = (video.split('-')[2].strip())
+    showtitle = ET.SubElement(episodedetails, "showtitle")
+    showtitle.text = (video.split('-')[2].strip())
+    aired = ET.SubElement(episodedetails, "aired")
+    aired.text = (air)
+    runtime = ET.SubElement(episodedetails, "runtime")
+    runtime.text = (lenght)
+    thumb = ET.SubElement(episodedetails, "thumb")
+    thumb.text = (video + ".jpg")
+
+    indent(episodedetails)
+
+    tree = ET.ElementTree(episodedetails)
+    tree.write((path+video+'.nfo'), xml_declaration=True, encoding='UTF-8', method='xml')
+
+# xml helper to create pretty xml
+# it basically walks the tree and adds spaces
+# and newlines so the tree is printed in a nice way
+def indent(elem, level=0):    
+    i = "\n" + level*"     "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "     "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+
+if __name__ == "__main__":
+    prefix, vurl = get_input(x_name, x_session, x_episode)
+    lenght, air = get_video(vurl, path, prefix)
+    video = convert_video(path, prefix)
+    xml_output(path, video, lenght, air)
+    os.system('rm {0}*.mp4'.format(path))
